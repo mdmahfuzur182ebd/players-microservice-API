afc480b1cd346eb2b43687bc600adf2d94beadd4
mahfuzur <mdmahfuazur7788@gamil.com>
2023-05-21 23:59:22 +0600

    First commit

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 
 
 
 
+
 const app = express()
+
+app.use(cors())
 app.use(morgan('dev'))
+app.use(express.json())
 
 
-
-app.get('/', (req,res) => {
+app.get('/', (_req,res) => {
     res.send("First API Create ")
+  //  res.status(200).json({status: 'OK'})
 })
 
 
